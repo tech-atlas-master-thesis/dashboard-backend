c47f9818f02ee83383f56449013647c9a9b598ec
Fabian Mooslechner <fabian@mooslechner.dev>
2026-05-29 11:13:50 +0200

    Allow all origins

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
